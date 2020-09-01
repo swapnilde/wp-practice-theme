090e6f81b25c81615c9965664000e5da64bf4ac4
swapnilde <swapnilde@bsf.io>
2020-09-01 10:33:14 +0530

    Added dynamic site title.

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 			}
 		?>
 		<img src="<?php echo $logo[0] ?>" width="30" height="30" class="d-inline-block align-top" alt="">
-		ACME
+		<?php echo get_bloginfo('name'); ?>
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
